365390165c1f878c420ec54d7042fc945cfad8ba
mayumis228-jaja <mayumi.s228@gmail.com>
2026-09-02 20:19:02 +0900

    Update manju_order_streamlit.py

--- manju_order_streamlit.py
+++ manju_order_streamlit.py
@@ -68,10 +68,9 @@ st.divider()
 col_btn1, col_btn2 = st.columns(2)
 
 with col_btn1:
-    if st.button("🔄 リセット", use_container_width=True):
-        for item in MENU.keys():
-            st.session_state[item] = 0
-        st.rerun()
+if st.button("🔄 リセット"):
+    st.session_state.clear()
+    st.rerun()
 
 with col_btn2:
     confirm = st.button(
